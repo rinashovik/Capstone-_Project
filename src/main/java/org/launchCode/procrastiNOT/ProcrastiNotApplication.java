981bac431faa5fb3b78be7396b4a63aab5c55e2e
rinashovik <rinashovik@gmail.com>
2023-12-08 11:43:23 -0600

    initialized spting boot application on GitHub

---
+++ src/main/java/org/launchCode/procrastiNOT/ProcrastiNotApplication.java
@@ -0,0 +1,13 @@
+package org.launchCode.procrastiNOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcrastiNotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcrastiNotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchCode/procrastiNOT/ProcrastiNotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchCode.procrastiNOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcrastiNotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
