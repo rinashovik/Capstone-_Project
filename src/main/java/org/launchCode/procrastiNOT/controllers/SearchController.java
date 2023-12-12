d23f068bf88e048980235d3a49cca9d7f44e61c4
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-12-12 16:00:26 -0500

    added attributes

--- src/main/java/org/launchCode/procrastiNOT/controllers/SearchController.java
+++ src/main/java/org/launchCode/procrastiNOT/controllers/SearchController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping
 public class SearchController {
     @RequestMapping("")
     public String search(Model model) {
-        model.addAttribute("columns");
+        List<String> columns = Arrays.asList("Column1", "Column2", "Column3");
+        model.addAttribute("columns", columns);
+
+        model.addAttribute("pageTitle", "Search Page");
         return "search";
     }
 }
