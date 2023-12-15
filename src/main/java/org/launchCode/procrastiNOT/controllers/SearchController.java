3b049e643ee7d91aa29afed6834078f2eb8c3f45
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-12-15 18:23:47 -0500

    SearchPageCreated

--- src/main/java/org/launchCode/procrastiNOT/controllers/SearchController.java
+++ src/main/java/org/launchCode/procrastiNOT/controllers/SearchController.java
@@ -1,21 +1,23 @@
 package org.launchCode.procrastiNOT.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Controller
-@RequestMapping
+@RequestMapping("search")
 public class SearchController {
-    @RequestMapping("")
+    /*@RequestMapping("")
     public String search(Model model) {
         List<String> columns = Arrays.asList("Column1", "Column2", "Column3");
         model.addAttribute("columns", columns);
 
         model.addAttribute("pageTitle", "Search Page");
+        return "search";
+    }*/
+
+    @RequestMapping("")
+    public String search(Model model) {
+        model.addAttribute("columns");
         return "search";
     }
 }
